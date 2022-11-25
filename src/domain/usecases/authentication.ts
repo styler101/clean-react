2a08142018ba37132c10806822160347a1af6aec
Lucas <lucas.santos@mundowap.com.br>
2022-11-24 21:25:12 -0300

    feat: add authentication use case

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,9 @@
+import { AccountModel } from './models/account'
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
+}
